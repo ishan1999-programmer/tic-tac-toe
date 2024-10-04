60d372442766a2a1bd81884e09601e278a67cbeb
ishan1999-programmer <ishantripathi16@gmail.com>
2024-10-04 21:03:19 +0530

    added custom color picker for X and O. Also added responsiveness to the color picker

--- app.js
+++ app.js
@@ -2,7 +2,33 @@ let content = document.querySelector(".content");
 let game_cells = document.querySelectorAll(".game > *");
 let game_chance_overlay = document.querySelector(".game_chance_overlay");
 let game_winner_overlay = document.querySelector(".game_winner_overlay");
-let reset  = document.querySelector("button");
+let reset  = document.querySelector("#reset_button");
+let color_of_x = document.querySelector("#color_of_x");
+let color_of_o = document.querySelector("#color_of_o");
+let x = document.querySelector("#x");
+let o = document.querySelector("#o");
+let start_button = document.querySelector("#start_button");
+let game_color_picker_overlay = document.querySelector(".game_color_picker_overlay");
+let xColor = "#0095cc";
+let oColor = "#f32e06";
+
+start_button.addEventListener("click" , (e)=>{
+    game_color_picker_overlay.style.transform = "translate(-500%,-50%)";
+    content.style.filter = "blur(0px)";
+})
+
+color_of_x.addEventListener("change" , (e)=>{
+    xColor = e.target.value;
+    x.style.color = `${e.target.value}`;
+    content.style.boxShadow = `0px 0px 15px ${xColor}`;
+    game_chance_overlay.style.backgroundColor = `${xColor}`;
+    game_chance_overlay.style.boxShadow = `0px 0px 5px ${xColor}`;
+});
+
+color_of_o.addEventListener("change" , (e)=>{
+    oColor = e.target.value;
+    o.style.color = `${e.target.value}`;
+});
 
 let chance_of = 'X';
 let count = 0;
@@ -15,7 +41,7 @@ function hanndleClick(e){
     if(chance_of=='X'){
         count++;
         e.target.innerText = 'X';
-        e.target.style.color = "rgb(0,126,204)";
+        e.target.style.color = `${xColor}`;
         e.target.style.pointerEvents = "none";
         if(isWin()){
             win();
@@ -26,14 +52,16 @@ function hanndleClick(e){
             return;
         }
         game_chance_overlay.classList.replace("left" , "right");
+        game_chance_overlay.style.backgroundColor = `${oColor}`;
+        game_chance_overlay.style.boxShadow = `0px 0px 5px ${oColor}`;
         game_chance_overlay.innerText = "O";
-        content.classList.replace("blue_shadow" , "red_shadow");
+        content.style.boxShadow = `0px 0px 15px ${oColor}`;
         chance_of = 'O';   
     }
     else{
         count++;
         e.target.innerText = 'O';
-        e.target.style.color = "rgb(243,46,6)";
+        e.target.style.color = `${oColor}`;
         e.target.style.pointerEvents = "none";
         if(isWin()){
             win();
@@ -44,8 +72,10 @@ function hanndleClick(e){
             return;
         }
         game_chance_overlay.classList.replace("right" , "left");
+        game_chance_overlay.style.backgroundColor = `${xColor}`;
+        game_chance_overlay.style.boxShadow = `0px 0px 5px ${xColor}`;
         game_chance_overlay.innerText = "X";
-        content.classList.replace("red_shadow" , "blue_shadow");
+        content.style.boxShadow = `0px 0px 15px ${xColor}`;
         chance_of = 'X';     
     }
 }
@@ -83,7 +113,7 @@ function isWin(){
 function win(){
     game_chance_overlay.classList.add("shrink");
     game_winner_overlay.innerText = `${chance_of} WON`;
-    chance_of=='X' ? game_winner_overlay.classList.add("xWon") : game_winner_overlay.classList.add("oWon");
+    game_winner_overlay.style.backgroundColor = chance_of=='X' ? `${xColor}` : `${oColor}`;
     game_winner_overlay.classList.remove("shrink");
     game_cells.forEach((cell)=>{
         cell.style.pointerEvents = "none";
@@ -92,17 +122,15 @@ function win(){
 
 function draw(){
     game_winner_overlay.innerText = "DRAW";
-    game_winner_overlay.classList.add("draw");
+    game_winner_overlay.style.backgroundColor = "#16cd50";
     game_chance_overlay.classList.add("shrink");
     game_winner_overlay.classList.remove("shrink");
-    content.classList.remove("blue_shadow");
-    content.classList.remove("red_shadow");
-    content.classList.add("green_shadow");
+    content.style.boxShadow = `0px 0px 15px #16cd50`;
 }
 
 reset.addEventListener("click" , resetGame);
 
-function resetGame(){
+function resetGame(e){
     chance_of = 'X';
     count = 0;
     game_cells.forEach((cell)=>{
@@ -111,15 +139,20 @@ function resetGame(){
     });
     game_chance_overlay.classList.remove("shrink");
     game_chance_overlay.classList.replace("right" , "left");
+    game_chance_overlay.style.backgroundColor = `#0095cc`;
+    game_chance_overlay.style.boxShadow = `0px 0px 5px #0095cc`;
     game_chance_overlay.innerText = "X";
     game_winner_overlay.classList.add("shrink");
     game_winner_overlay.innerText = "";
-    game_winner_overlay.classList.remove("xWon");
-    game_winner_overlay.classList.remove("oWon");
-    game_winner_overlay.classList.remove("draw");
-    content.classList.remove("green_shadow");
-    content.classList.remove("red_shadow");
-    content.classList.add("blue_shadow");
+    content.style.boxShadow = `0px 0px 15px #0095cc`;
+    game_color_picker_overlay.style.transform = "translate(-50%,-50%)";
+    content.style.filter = "blur(15px)";
+    x.style.color = "#0095cc";
+    o.style.color = "#f32e06";
+    color_of_x.value = "#0095cc";
+    color_of_o.value = "#f32e06";
+    xColor = "#0095cc";
+    oColor = "#f32e06";
 }
 
 
